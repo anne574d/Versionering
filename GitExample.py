36e539b0f90b75ab18afe58e758d4e0e61ba0de9
anne574d <anne574d@edu.sde.dk>
2019-09-04 10:53:59 +0200

    branched to object oriented version

--- GitExample.py
+++ GitExample.py
@@ -1,12 +1,36 @@
 import os
 import subprocess
 
-cmd = "git status"
-output = os.popen(cmd).read()
+class GitOutput:
+	def __init__(self, cmd):
+		self.output = os.popen("git " + cmd).read()
 
-if output.find("Untracked files:"") is not -1:
-	print("Du har filer du mangler at tilføje til dit repository!")
-	
-if output.find("Changes to be committed:") is not -1:
-	print("Du har filer hvis ændringer ikke er blevet committet")
-	
+	def IsInRepository(self):
+		index = self.output.find("not a git repository")
+		return (index is -1)
+
+	def HasUntrackedFiles(self):
+		index = self.output.find("Untracked files:")
+		return (index is not -1)
+
+	def HasChangesToBeCommitted(self):
+		index = self.output.find("Changes to be committed:")
+		return (index is not -1)
+
+	def HasUnstagedChanges(self):
+		index = self.output.find("Changes not staged for commit:")
+		return (index is not -1)
+
+gitter = GitOutput("status")
+
+if not gitter.IsInRepository():
+	print("Dette er ikke et git repository")
+else:
+	if gitter.HasUntrackedFiles():
+		print("Der er untracket filer i dette repository")
+	elif gitter.HasChangesToBeCommitted():
+		print("Der er ucommittet ændringer i dette repository")
+	elif gitter.HasUnstagedChanges():
+		print("Der er unstaged ændringer i dette repository")
+	else:
+		print("Alt er vel her...")
